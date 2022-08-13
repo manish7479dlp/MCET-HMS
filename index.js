21b89e9b14afb3743409ca849374564fd47a29f3
Manish Kumar <manish7479dlp@gmail.com>
2022-08-13 23:25:08 +0530

    Add: middleware

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const partialsPath = path.join(__dirname , "template/partials");
 
 app.use(express.json())
 app.use(cors());
+app.use(express.urlencoded({extended: false}))
 
 app.use(express.static(staticPath))
 app.set("view engine" , "hbs");
