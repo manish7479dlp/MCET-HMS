55e33397a37cef458346b5c3b7c1c5c071b09d9e
Manish Kumar <manish7479dlp@gmail.com>
2022-08-16 18:25:59 +0530

    Add: find blank room details of a particular room code implemented

--- public/seatAvailableJs.js
+++ public/seatAvailableJs.js
@@ -1,6 +1,8 @@
 const BlankSeat = document.getElementsByClassName("blankSeat");
 const checkSeatAvailability = async () => {
   try {
+    document.getElementsByClassName("seatsAvailableContainer")[0].style.display = "flex";
+    document.getElementsByClassName("seatsAvailableContainer")[1].style.display = "flex";
     const SeatSearchParameter = document.getElementById("seatSearchParameter");
     const url = "http://localhost:8000";
     let originalUrl = "student";
@@ -10,63 +12,95 @@ const checkSeatAvailability = async () => {
     const response = await fetch(originalUrl);
     const result = await response.json();
     // console.log(result);
-     
-    BlankSeat[0].style.display = "none"
-    BlankSeat[1].style.display = "none"
-    BlankSeat[2].style.display = "none"
-    BlankSeat[3].style.display = "none"
-    BlankSeat[4].style.display = "none"
-    BlankSeat[5].style.display = "none"
-    BlankSeat[6].style.display = "none"
-    BlankSeat[7].style.display = "none"
-    BlankSeat[8].style.display = "none"
-    
+
+    document.getElementById("upperRoom").innerText = `Building Number: ${SeatSearchParameter.value}A`
+    document.getElementById("lowerRoom").innerText = `Building Number: ${SeatSearchParameter.value}`
+
+    BlankSeat[0].style.display = "none";
+    BlankSeat[1].style.display = "none";
+    BlankSeat[2].style.display = "none";
+    BlankSeat[3].style.display = "none";
+    BlankSeat[4].style.display = "none";
+    BlankSeat[5].style.display = "none";
+    BlankSeat[6].style.display = "none";
+    BlankSeat[7].style.display = "none";
+
     setDataToStudent(result, SeatSearchParameter.value);
-    SeatSearchParameter.value = ""
+    SeatSearchParameter.value = "";
   } catch (error) {
     console.log(error);
   }
 };
 
-
-
 const setDataToStudent = (data, buildingNumber) => {
-
-    // upper 2-seaters
+  // upper 2-seaters
   const u2fName = document.getElementById("u2fName");
   const u2fYear = document.getElementById("u2fYear");
   const u2fDept = document.getElementById("u2fDept");
 
+  u2fName.style.display = "block"
+  u2fYear.style.display = "block"
+  u2fDept.style.display = "block"
+
   const u2sName = document.getElementById("u2sName");
   const u2sYear = document.getElementById("u2sYear");
-  const u2sDept = document.getElementById("u2sDept")
+  const u2sDept = document.getElementById("u2sDept");
 
-//   upper 3-seaters
+  u2sName.style.display = "block"
+  u2sYear.style.display = "block"
+  u2sDept.style.display = "block"
+
+  //   upper 3-seaters
   const u3fName = document.getElementById("u3fName");
   const u3fYear = document.getElementById("u3fYear");
   const u3fDept = document.getElementById("u3fDept");
+
+  u3fName.style.display = "block"
+  u3fYear.style.display = "block"
+  u3fDept.style.display = "block"
+  
 
   const u3sName = document.getElementById("u3sName");
   const u3sYear = document.getElementById("u3sYear");
   const u3sDept = document.getElementById("u3sDept");
 
-//   lower 2-seaters
+  u3sName.style.display = "block"
+  u3sYear.style.display = "block"
+  u3sDept.style.display = "block"
+
+  //   lower 2-seaters
   const l2fName = document.getElementById("l2fName");
   const l2fYear = document.getElementById("l2fYear");
   const l2fDept = document.getElementById("l2fDept");
 
+  l2fName.style.display = "block"
+  l2fYear.style.display = "block"
+  l2fDept.style.display = "block"
+
   const l2sName = document.getElementById("l2sName");
   const l2sYear = document.getElementById("l2sYear");
   const l2sDept = document.getElementById("l2sDept");
-    
-//   lower 3-seaters
+
+  l2sName.style.display = "block"
+  l2sYear.style.display = "block"
+  l2sDept.style.display = "block"
+
+  //   lower 3-seaters
   const l3fName = document.getElementById("l3fName");
   const l3fYear = document.getElementById("l3fYear");
   const l3fDept = document.getElementById("l3fDept");
 
+  l3fName.style.display = "block"
+  l3fYear.style.display = "block"
+  l3fDept.style.display = "block"
+
   const l3sName = document.getElementById("l3sName");
   const l3sYear = document.getElementById("l3sYear");
   const l3sDept = document.getElementById("l3sDept");
+
+  l3sName.style.display = "block"
+  l3sYear.style.display = "block"
+  l3sDept.style.display = "block"
 
   let flag = false;
 
@@ -76,20 +110,17 @@ const setDataToStudent = (data, buildingNumber) => {
     );
   });
 
-// console.log(lower2SeaterData);
+  // console.log(lower2SeaterData);
 
-if(lower2SeaterData.length === 2) {
-
+  if (lower2SeaterData.length === 2) {
     l2fName.lastChild.innerText = lower2SeaterData[0].name;
     l2fYear.lastChild.innerText = lower2SeaterData[0].year;
     l2sDept.lastChild.innerText = lower2SeaterData[0].department;
 
     l2sName.lastChild.innerText = lower2SeaterData[1].name;
-    l2sYear.lastChild.innerText = lower2SeaterData[2].year;
-    l2sDept.lastChild.innerText = lower2SeaterData[3].department;
-
-
-} else if(lower2SeaterData.length === 1) {
+    l2sYear.lastChild.innerText = lower2SeaterData[1].year;
+    l2sDept.lastChild.innerText = lower2SeaterData[1].department;
+  } else if (lower2SeaterData.length === 1) {
     l2fName.lastChild.innerText = lower2SeaterData[0].name;
     l2fYear.lastChild.innerText = lower2SeaterData[0].year;
     l2fDept.lastChild.innerText = lower2SeaterData[0].department;
@@ -102,71 +133,70 @@ if(lower2SeaterData.length === 2) {
     l2sYear.style.display = "none";
     l2sDept.style.display = "none";
 
-} else {
-    l2fName.innerHTML = "Empty"
-    l2fYear.innerHTML = ""
-    l2fDept.innerHTML = ""
+    BlankSeat[5].style.display = "block";
+  } else {
+    l2fName.style.display = "none";
+    l2fYear.style.display = "none";
+    l2fDept.style.display = "none";
+    BlankSeat[4].style.display = "block";
 
-    l2sName.innerHTML = "Empty"
-    l2sYear.innerHTML = ""
-    l2sDept.innerHTML = ""
-}
-
+    l2sName.style.display = "none";
+    l2sYear.style.display = "none";
+    l2sDept.style.display = "none";
+    BlankSeat[5].style.display = "block";
+  }
 
   const lower3SeaterData = data.filter((data) => {
     return (
-      data.roomType === "2-seaters" && data.buildingNumber === buildingNumber
+      data.roomType === "3-seaters" && data.buildingNumber === buildingNumber
     );
   });
 
-// console.log(lower3SeaterData);
+  // console.log(lower3SeaterData);
 
-if(lower3SeaterData.length === 2) {
-
-    l3fName.lastChild.innerText = lower3SeaterData[0].name;
-    l3fName.lastChild.innerText = lower3SeaterData[0].year;
-    l3fName.lastChild.innerText = lower3SeaterData[0].department;
-
-    l3sName.lastChild.innerText = lower3SeaterData[1].name;
-    l3sName.lastChild.innerText = lower3SeaterData[2].year;
-    l3sName.lastChild.innerText = lower3SeaterData[3].department;
-
-
-} else if(lower3SeaterData.length === 1) {
+  if (lower3SeaterData.length === 2) {
     l3fName.lastChild.innerText = lower3SeaterData[0].name;
     l3fYear.lastChild.innerText = lower3SeaterData[0].year;
     l3fDept.lastChild.innerText = lower3SeaterData[0].department;
 
-    l3sName.innerHTML = "Empty"
-    l3sYear.innerHTML = ""
-    l3sDept.innerHTML = ""
+    l3sName.lastChild.innerText = lower3SeaterData[1].name;
+    l3sYear.lastChild.innerText = lower3SeaterData[1].year;
+    l3sDept.lastChild.innerText = lower3SeaterData[1].department;
+  } else if (lower3SeaterData.length === 1) {
+    l3fName.lastChild.innerText = lower3SeaterData[0].name;
+    l3fYear.lastChild.innerText = lower3SeaterData[0].year;
+    l3fDept.lastChild.innerText = lower3SeaterData[0].department;
 
-} else {
-    l3fName.innerHTML = "Empty"
-    l3fYear.innerHTML = ""
-    l3fDept.innerHTML = ""
+    l3sName.style.display = "none";
+    l3sYear.style.display = "none";
+    l3sDept.style.display = "none";
 
-    l3sName.innerHTML = "Empty"
-    l3sYear.innerHTML = ""
-    l3sDept.innerHTML = ""
-}
-  
+    BlankSeat[7].style.display = "block";
+  } else {
+    l3fName.style.display = "none";
+    l3fYear.style.display = "none";
+    l3fDept.style.display = "none";
+    BlankSeat[6].style.display = "block";
+
+    l3sName.style.display = "none";
+    l3sYear.style.display = "none";
+    l3sDept.style.display = "none";
+    BlankSeat[7].style.display = "block"
+  }
 
   buildingNumber = buildingNumber + "A";
 
-//   console.log(buildingNumber);
-
+  //   console.log(buildingNumber);3
   const upper2SeaterData = data.filter((data) => {
     return (
       data.roomType === "2-seaters" && data.buildingNumber === buildingNumber
     );
-  });
+  })
 
-console.log(upper2SeaterData);
-console.log(upper2SeaterData.length);
+  console.log(upper2SeaterData);
+  console.log(upper2SeaterData.length);
 
-if(upper2SeaterData.length === 2) {
-
+  if (upper2SeaterData.length === 2) {
     u2fName.lastChild.innerText = upper2SeaterData[0].name;
     u2fName.lastChild.innerText = upper2SeaterData[0].year;
     u2fName.lastChild.innerText = upper2SeaterData[0].department;
@@ -174,29 +204,28 @@ if(upper2SeaterData.length === 2) {
     u2sName.lastChild.innerText = upper2SeaterData[1].name;
     u2sName.lastChild.innerText = upper2SeaterData[1].year;
     u2sName.lastChild.innerText = upper2SeaterData[1].department;
-
-
-} else if(upper2SeaterData.length === 1) {
+  } else if (upper2SeaterData.length === 1) {
     console.log(upper2SeaterData[0].name);
     u2fName.lastChild.innerText = upper2SeaterData[0].name;
     u2fYear.lastChild.innerText = upper2SeaterData[0].year;
     u2fDept.lastChild.innerText = upper2SeaterData[0].department;
 
-    u2sName.innerHTML = "Empty"
-    u2sYear.innerHTML = ""
-    u2sDept.innerHTML = ""
+    u2sName.style.display = "none";
+    u2sYear.style.display = "none";
+    u2sDept.style.display = "none";
 
-} else {
-    u2fName.innerHTML = "Empty"
-    u2fYear.innerHTML = ""
-    u2fDept.innerHTML = ""
+    BlankSeat[1].style.display = "block";
+  } else {
+    u2fName.style.display = "none";
+    u2fYear.style.display = "none";
+    u2fDept.style.display = "none";
+    BlankSeat[0].style.display = "block";
 
-    u2sName.innerHTML = "Empty"
-    u2sYear.innerHTML = ""
-    u2sDept.innerHTML = ""
-}
-
-
+    u2sName.style.display = "none";
+    u2sYear.style.display = "none";
+    u2sDept.style.display = "none";
+    BlankSeat[1].style.display = "block"
+  }
 
   const upper3SeaterData = data.filter((data) => {
     return (
@@ -204,35 +233,35 @@ if(upper2SeaterData.length === 2) {
     );
   });
 
-//   console.log( upper3SeaterData);
+  //   console.log( upper3SeaterData);
 
-  if(upper3SeaterData.length === 2) {
-
+  if (upper3SeaterData.length === 2) {
     u3fName.lastChild.innerText = upper3SeaterData[0].name;
     u3fYear.lastChild.innerText = upper3SeaterData[0].year;
     u3fDept.lastChild.innerText = upper3SeaterData[0].department;
 
-    u3sName.lastChild.innerText = upper3SeaterData[0].name;
-    u3sYear.lastChild.innerText = upper3SeaterData[0].year;
-    u3sDept.lastChild.innerText = upper3SeaterData[0].department;
-
-
-} else if(upper3SeaterData.length === 1) {
+    u3sName.lastChild.innerText = upper3SeaterData[1].name;
+    u3sYear.lastChild.innerText = upper3SeaterData[1].year;
+    u3sDept.lastChild.innerText = upper3SeaterData[1].department;
+  } else if (upper3SeaterData.length === 1) {
     u3fName.lastChild.innerText = upper3SeaterData[0].name;
     u3fYear.lastChild.innerText = upper3SeaterData[0].year;
     u3fDept.lastChild.innerText = upper3SeaterData[0].department;
 
-    u3sName.innerHTML = "Empty"
-    u3sYear.innerHTML = ""
-    u3sDept.innerHTML = ""
+    u3sName.style.display = "none";
+    u3sYear.style.display = "none";
+    u3sDept.style.display = "none";
 
-} else {
-    u3fName.innerHTML = "Empty"
-    u3fYear.innerHTML = ""
-    u3fDept.innerHTML = ""
+    BlankSeat[3].style.display = "block";
+  } else {
+    u3fName.style.display = "none";
+    u3fYear.style.display = "none";
+    u3fDept.style.display = "none";
+    BlankSeat[2].style.display = "block";
 
-    u3sName.innerHTML = "Empty"
-    u3sYear.innerHTML = ""
-    u3sDept.innerHTML = ""
-}
+    u3sName.style.display = "none";
+    u3sYear.style.display = "none";
+    u3sDept.style.display = "none";
+    BlankSeat[3].style.display = "block"
+  }
 };
