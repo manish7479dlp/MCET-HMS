aa4b04d260636d85b9064de79fd3b4fd6e1cd0eb
Manish Kumar <manish7479dlp@gmail.com>
2022-08-24 01:09:15 +0530

    Add: new mongodb atlas link is added

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
-const dburl = "mongodb+srv://manish7479dlp:WGgwv3gheMJa6bqv@cluster0.xlyrwg7.mongodb.net/?retryWrites=true&w=majority"
-// const dburl = "mongodb://localhost:27017/College-HMS"
 
-mongoose.connect(dburl).then(() => {
+const dburl =
+  "mongodb+srv://mcethms2022:AePueEu1ipCJOj2m@cluster0.ciqn55k.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dburl)
+  .then(() => {
     console.log("DataBase Connection Established.");
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
